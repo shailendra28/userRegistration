8c3f4a235bb5194f121b431fc7a712ea3ad224dc
shailendra28 <yshailendrakumar1768@gmail.com>
2021-05-10 04:08:19 +0530

    ADDED: User REgistraction

---
+++ UserRegistration.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+
+public class UserRegistration {
+    private boolean check;
+    private String firstName;
+    Scanner sc = new Scanner(System.in);
+    public void checkFirstName() {
+        System.out.println("enter first name starts with cap and has minimum 3 character : ");
+        firstName = sc.nextLine();
+        check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(firstName).matches();
+        if (!check) {
+            System.out.println(" first name is invalid ");
+            checkFirstName();
+        }
+        else {
+            System.out.println(" first name is valid ");
+        }
+    }
+
+    public static void main(String args[]) {
+        UserRegistration user = new UserRegistration();
+        user.checkFirstName();
+    }
+
+
+}
