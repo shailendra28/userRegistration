dcb87acafa7dbc2c51eb6a293f9fa8d7de28fc78
shailendra28 <yshailendrakumar1768@gmail.com>
2021-05-10 04:15:04 +0530

    User Need To Enter Valid First Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -10,7 +10,7 @@ public class UserRegistration {
         System.out.println("enter first name starts with cap and has minimum 3 character : ");
         firstName = sc.nextLine();
         check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(firstName).matches();
-        if (!check) {
+        if(!check){
             System.out.println(" first name is invalid ");
             checkFirstName();
         }
