4c60aa044fce3251219577be01cb62af92eda376
shailendra28 <yshailendrakumar1768@gmail.com>
2021-05-10 08:34:34 +0530

    ADDED:

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     private boolean check;
-    private String firstName;
+    private String firstName,lastName;
     Scanner sc = new Scanner(System.in);
     public void checkFirstName() {
         System.out.println("enter first name starts with cap and has minimum 3 character : ");
@@ -18,10 +18,21 @@ public class UserRegistration {
             System.out.println(" first name is valid ");
         }
     }
-
+    public void checkLastName() {
+        System.out.println("enter last name starts with cap and has minimum 3 character : ");
+        lastName = sc.nextLine();
+        check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(lastName).matches();
+        if (!check) {
+            System.out.println(" last name is invalid ");
+            checkLastName();
+        }
+        else
+            System.out.println(" last name is valid ");
+    }
     public static void main(String args[]) {
         UserRegistration user = new UserRegistration();
         user.checkFirstName();
+        user.checkLastName();
     }
 
 
